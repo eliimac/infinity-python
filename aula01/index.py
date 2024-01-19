9c8f450b2e8fc8b3048ea439a2c8940d7c7a9a32
Cami <146574739+eliimac@users.noreply.github.com>
2024-01-19 19:55:05 -0300

    Add files via upload

---
+++ aula01/index.py
@@ -0,0 +1,25 @@
+# print("cadastro de senha")
+# print("_"*60)
+
+# senhaCad = input("Digite a senha: ")
+# senhaConf = input("Confirme a senha: ")
+
+# while senhaCad != senhaConf:
+#     senhaConf = input("senhas diferentes, digite novamente: ")
+# print("senha cadastrada")    
+
+# print("_"*60)
+# print('login')
+
+# senhaDig = input("Digite a senha para logar: ")
+
+# if senhaDig == senhaCad:
+#     print("Desbloqueado!")
+
+# else:
+#     for numeroTent in range(2, 4):
+#         senhaDig = input(f"Senha errada, tentativa {numeroTent}, digite novamente: ")
+#         if senhaDig == senhaCad:
+#             print("Bem vindo")         
+#             break   
+#             print("Bloqueado!")
---
+++ aula01/index01.py
@@ -0,0 +1,111 @@
+# # Faça um programa em python que determine em duas variáveis (senha e email) e que contenha uma senha e um email cadastrados antecipadamente, em seguida solicite ao usuário uma senha e um email e utilize o laço de repetição juntamente com a estrutura de condição para verificar se o email e a senha digitado pelo usuário é igual ao email e senha cadastrados antecipadamente. E enquanto a senha e o email que o usuário digitou não for igual ao email e senha cadastrados ele continue em um loop.
+
+# print("cadastro")
+# print("_"*60)
+
+# emailCad = input('Cadastre o email: ')
+# emailConf = input('Confirme o email: ')
+# print("_"*60)
+# senhaCad = input('Cadastre a senha: ')
+# senhaConf = input('Confirme a senha: ')
+# print("_"*60)
+
+# while emailCad.lower() != emailConf.lower() or senhaCad.lower() != senhaConf.lower():
+#     print('Email ou senha diferente, digite novamente')
+#     emailConf = input('Confirme o email: ')
+#     senhaConf = input('Confirme a senha: ')
+
+# print("_"*60)
+# print('Cadastro feito com sucesso!')   
+# print("_"*60)
+
+# emailDig = input('Digite o email para logar: ')
+# senhaDig = input('Digite a senha para logar: ')
+
+# if emailDig.lower() == emailCad.lower() and senhaDig.lower() == senhaCad.lower():
+#     print('Bem vindo.')
+
+# else:
+#     for numeroTent in range(1, 10):
+#         print('Email ou senha diferente, digite novamente')
+#         emailDig = input('Digite o email para logar: ')
+#         senhaDig = input('Digite a senha para logar: ')
+#         if emailDig.lower() == emailCad.lower() and senhaDig.lower() == senhaCad.lower():
+#             break
+#     print('Bem vindo')       
+           
+
+# palavra = "infinity school"
+
+# contador = 0
+
+
+
+# for letra in palavra:
+#     if letra in 'aeiou':
+#         contador += 1           
+
+#         print(contador)
+
+x = 10
+
+y = 5
+
+
+
+# 
+
+# n = 1
+
+# produto = 1
+
+
+
+# while n <= 5:
+
+#     produto = produto*n
+
+#     n += 1
+
+#     print(produto)
+
+
+# x = 15
+
+# y = 7
+
+
+
+# resultado = ""
+
+
+
+# if x > 10:
+
+#     resultado += "A"
+
+# else:
+
+#     resultado += "B"
+
+
+
+# if y < 5:
+
+#     resultado += "C"
+
+# else:
+
+#     resultado += "D"
+
+# x = 15
+
+# y = 10
+
+# z = 20
+
+
+
+# resultado = (x > y) and (y < z)
+
+# print(resultado)
